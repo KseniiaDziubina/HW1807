86a5a8a80f9fd1d9883e7558f2c955e89e537fbb
Kseniia <ksenjadzjubina@gmail.com>
2022-07-18 16:58:08 +0300

    Вариация третьей задачи

---
+++ Example0032/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int Day;
+Day = Convert.ToInt32(Console.ReadLine());
+if (Day <=5){
+    Console.WriteLine("Нет, это рабочий день");
+}
+else if (Day <=7){
+    Console.WriteLine("Да, это выходной день");
+}
+Console.ReadLine();
